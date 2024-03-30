df9f352743d9f3c55cf1a36fc2fcc3aad4ecb4de
evasyukov <egor.vasyukov1@gmail.com>
2024-03-30 11:39:16 +0700

    add component  on parent component

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,29 @@
 <template>
-  <div class="menu">
-    <button>10 sec</button>
-    <button>30 sec</button>
-    <button>60 sec</button>
-  </div>
-  <div id="click" class="reaction"></div>
+    <Clicker @test-result="showResult" />
+    <Result v-if="result" :clicks="result" />
 </template>
 
 <script>
+import Clicker from "./components/Clicker.vue"
+import Result from "./components/Result.vue"
+
 export default {
-  components: {},
+  name: "App",
+  components: {
+    Clicker,
+    Result,
+  },
+  data() {
+    return {
+      result: null,
+    }
+  },
+  methods: {
+    showResult(clicks) {
+      this.result = clicks
+    },
+  },
 }
 </script>
 
-<style scoped>
-.main {
-  display: flex;
-}
-button {
-  width: 100px;
-  height: 100px;
-  margin: 10px;
-  border: 0;
-  cursor: pointer;
-  background: rgba(255, 255, 255, 0.4);
-  backdrop-filter: blur(100px);
-  border-radius: 50%;
-}
-button:active {
-  background: #e30a0a;
-  transition: 0.3s;
-}
-.reaction {
-  width: 200px;
-  height: 200px;
-  background-color: aqua;
-}
-</style>
+<style scoped></style>
