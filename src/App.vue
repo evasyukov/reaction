2b5ab78daef836d41a56537f813c10aff168316c
evasyukov <egor.vasyukov1@gmail.com>
2024-03-30 10:40:47 +0700

    create project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="main">
+
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  components: {
+
+  },
+}
+</script>
+
+<style scoped>
+.main{
+  display: flex;
+}
+
+</style>
