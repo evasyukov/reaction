1098a6e7d59fb50f6506b31bd9c5e67e45722177
evasyukov <egor.vasyukov1@gmail.com>
2024-03-30 11:37:11 +0700

    test time selection

---
+++ src/components/Time.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <button @click="$emit('select-time', 10)">10 секунд</button>
+    <button @click="$emit('select-time', 30)">30 секунд</button>
+    <button @click="$emit('select-time', 60)">60 секунд</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Time",
+}
+</script>
+
+<style>
+button {
+  width: 100px;
+  height: 100px;
+  margin: 10px;
+  border: 0;
+  cursor: pointer;
+  background: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(100px);
+  border-radius: 50%;
+}
+button:active {
+  background: #e30a0a;
+  transition: 0.3s;
+}
+</style>
