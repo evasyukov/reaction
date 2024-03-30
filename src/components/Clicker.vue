dfd38f8ee79aebfa7ba8cd300003384091eabe38
evasyukov <egor.vasyukov1@gmail.com>
2024-03-30 11:37:53 +0700

    add template clicker

---
+++ src/components/Clicker.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <Time @select-time="setTime"></Time>
+    <div v-if="time">
+      <p>Время: {{ time }} секунд</p>
+      <button @click="startTest">Начать тест</button>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
