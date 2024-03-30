63190a3021d0073547f99b6e96f7472ef74419bf
evasyukov <egor.vasyukov1@gmail.com>
2024-03-30 11:38:36 +0700

    add component for result clicker test

---
+++ src/components/Result.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+       <h3>Результат теста:</h3>
+       <p>Количество кликов: {{ clicks }}</p>
+    </div>
+   </template>
+   
+   <script>
+   export default {
+    name: 'Result',
+    props: {
+       clicks: {
+         type: Number,
+         required: true
+       }
+    }
+   }
+   </script>
